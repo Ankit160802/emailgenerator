11038ba18929b1bb2bfd8b3727432e098178688f
Ankit160802 <142831535+Ankit160802@users.noreply.github.com>
2024-07-18 10:01:54 +0530

    Update emailapp.py

--- emailapp.py
+++ emailapp.py
@@ -13,43 +13,43 @@ from transformers import AutoModelForCausalLM
 
 
 
-HF_TOKEN="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"
+# HF_TOKEN="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"
 
-# Quantisation
-bnb_config=BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
-
-
-model_name="meta-llama/Llama-2-7b-chat-hf"
-
-tokenizer=AutoTokenizer.from_pretrained(model_name,token=HF_TOKEN)
-tokenizer.pad_token=tokenizer.eos_token
+# # Quantisation
+# bnb_config=BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_use_double_quant=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype=torch.bfloat16
+# )
 
 
+# model_name="meta-llama/Llama-2-7b-chat-hf"
 
-model=AutoModelForCausalLM.from_pretrained(
-    model_name,
-    device_map="auto",
-    quantization_config=bnb_config,
-    token=HF_TOKEN
-)
-
-# PIPELINE
-text_generator=pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    max_new_tokens=128
-)
+# tokenizer=AutoTokenizer.from_pretrained(model_name,token=HF_TOKEN)
+# tokenizer.pad_token=tokenizer.eos_token
 
 
 
-llm=HuggingFacePipeline(pipeline=text_generator)
+# model=AutoModelForCausalLM.from_pretrained(
+#     model_name,
+#     device_map="auto",
+#     quantization_config=bnb_config,
+#     token=HF_TOKEN
+# )
 
+# # PIPELINE
+# text_generator=pipeline(
+#     "text-generation",
+#     model=model,
+#     tokenizer=tokenizer,
+#     max_new_tokens=128
+# )
+
+
+
+# llm=HuggingFacePipeline(pipeline=text_generator)
+llm=Ollama(model="llama3")
 
 def generate_email(subject1):
 
